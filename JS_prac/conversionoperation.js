cca7423ef3f5a60ffe034af8f2b8cdf1fba2b718
thetelgote <sushanttelgote4@gmail.com>
2024-06-13 11:06:12 +0000

    this is an third updated code

---
+++ JS_prac/conversionoperation.js
@@ -0,0 +1,23 @@
+let score = "199abc"
+
+console.log(typeof(score));
+
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+
+console.log( valueInNumber);
+
+//"33"=> number
+//"33abc"=> NaN not an number
+
+let someNumber= 33
+
+let stringNumber = String(someNumber);
+
+console.log( typeof stringNumber)
+
+
+
+
+
