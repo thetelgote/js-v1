7c439c370e4fef2347b463f4404aed3d20d79563
thetelgote <sushanttelgote4@gmail.com>
2024-08-06 23:07:59 +0530

    Create date.js

---
+++ JS_prac/date.js
@@ -0,0 +1,20 @@
+let myDate = new Date ()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+console.log(typeof myDate);
+
+
+
+// let myCreateDate = new Date(2023,0,23); //format 1 
+
+
+// console.log(myCreateDate.toDateString());
+
+let mynew = new Date("01-14-2023") //format 2 create date 
+
+console.log(mynew.toDateString())
+
+console.log(TimeStamp());
+
+console.log(Date.now());
