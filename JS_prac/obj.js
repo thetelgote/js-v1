c5fb93ae1402cfecc69b991a7b01d9aeb5511457
thetelgote <sushanttelgote4@gmail.com>
2024-08-06 23:06:21 +0530

    Create obj.js

---
+++ JS_prac/obj.js
@@ -0,0 +1,52 @@
+// const car = {type:"SUV",Company:"Suzuki"  }
+
+
+// const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+
+// console.log(car)
+// console.log(person)
+
+
+// const employee = {};
+// employee.name = "Sushant";
+// employee.role = "Software Engineer";
+// employee.SalarySalary="700000";
+
+// console.log(employee)
+
+
+// myObj = {
+//     name:"John",
+//     age:30,
+//     myCars: {
+//       car1:"Ford",
+//       car2:"BMW",
+//       car3:"Fiat",
+
+   
+
+
+
+//     greet : function(){
+//       console.log("Helo ")
+
+//      }
+    
+
+//     }
+//   }
+
+// console.log(myObj.myCars.greet())
+// console.log(myObj.myCars["car2"]);
+
+// console.log(myObj.myCars.car2); // 2ND ACESSING METHOD 
+
+
+
+
+function Person(first, last, age, eye) {
+    this.firstName = "sushant";
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eye;
+  }
