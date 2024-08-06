9f70562c0e8b81c0b5620a9149c133c3dd8aab4a
thetelgote <sushanttelgote4@gmail.com>
2024-08-06 23:10:20 +0530

    Create var2.js

---
+++ JS_prac/var2.js
@@ -0,0 +1,11 @@
+console.log("hii")
+//const is use if value not change 
+const a = 10;
+console.log(a)
+// a  = 29;
+//if the const not use the use always use  let
+let b = "30"
+
+b = 33;
+
+console.log(b)
